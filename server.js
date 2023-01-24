fcf3c9e7b0a8ba6063986b6a920dd64c9954b93d
ngogatishvili <ngogatishvili@websmart.gmail.com>
2023-01-24 14:28:37 +0400

    commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const { BADFAMILY } = require("dns")
+const { readdirSync } = require("fs")
+const http=require("http")
+
+const todos=[
+    {id:1,text:"Todo one"},
+    {id:2,text:"Todo two"},
+    {id:3,text:"Todo three"},
+]
+const server=http.createServer((req,res)=>{
+    const {method,url}=req;
+
+    let body=[];
+
+    req.on("data",chunk=>{
+        body.push(chunk);
+    }).on("end",()=>{
+        body=Buffer.concat(body).toString();
+        let status=404;
+        const response={
+            success:false,
+            data:null,
+            error:null
+        }
+
+        if(method==="GET" && url==="/todos") {
+            status=200;
+            response.success=true;
+            response.data=todos;
+        }else if(method==="POST" && url==="/todos") {
+            const {id,text}=JSON.parse(body);
+            if(!id||!text) {
+                status=400;
+                response.error="Please add id and text"
+            }else{
+                todos.push({id,text})
+            status=201;
+            response.success=true;
+            response.data=todos;
+            }
+        }  
+        res.writeHead(status,{
+            "Content-Type":"application/json",
+            "X-Powered-By":"Node.js"
+        })
+        res.end(JSON.stringify(response));
+    })
+
+})
+
+const PORT=8000;
+
+server.listen(PORT,()=>{
+    console.log(`Server running at ${PORT}`)
+})
